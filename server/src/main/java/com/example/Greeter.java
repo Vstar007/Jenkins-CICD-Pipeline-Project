7b409ac0bfd94af42098d551da0aacc80b69c5f0
victor <animvictor556@gmail.com>
2022-11-10 18:20:47 -0700

    changes

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,18 +4,18 @@ package com.example;
  * This is a class.
  */
 public class Greeter {
+
   /**
    * This is a constructor.
    */
   public Greeter() {
+
   }
 
   /**
    * This is a method.
    */
   public final String greet(final String someone) {
-    String password = "Admin@12345";
-    System.out.println("Hello");
     return String.format("Hello Avinash, %s!", someone);
   }
 }
